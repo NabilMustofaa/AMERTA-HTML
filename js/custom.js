af6912e03b3da7a94981dd8e4fd5289894247802
nabilmustofaa <nabilmustofa6@gmail.com>
2022-07-27 14:21:35 +0700

    finish

---
+++ js/custom.js
@@ -0,0 +1,76 @@
+
+
+jQuery(function ($) {
+    $(document).ready(function(){
+        $("#play").on("load", function(){
+            $(this).contents().on("mousedown, mouseup, click", function(){
+                alert("Click detected inside iframe.");
+            });
+        });
+    });
+    // $(document).ready(function () {
+    //     $('#play').click(function(event) { 
+    //         console.log('play');
+    //         $(this).removeClass('filter')
+    //      });
+    //    });
+
+    /* ===================================
+    Header Appear On Scroll
+    ====================================== */
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 70) { // Set position from top to add class
+            $('header').addClass('sticky header-appear');
+            $('.left-logo .navbar-brand').addClass("display_none");
+        }
+        else {
+            $('header').removeClass('sticky header-appear');
+            $('.left-logo .navbar-brand').removeClass("display_none");
+        }
+    });
+
+    $(window).on('scroll', function () {
+        var position = $(this).scrollTop(); //position scrolled sofar
+        
+        $('section').each(function() { //for each loop(chcecks for every element with that class)
+        //for your every section on site or your class of main sections you are targetting
+            var target = $(this).offset().top-10;//each element position from top
+            var targetBot = target + $(this).height()+100//each element position from top + height of element
+            var id = $(this).attr('id');//this element ID - should be same as data-id on your nav link
+            $('.navbar-nav li#'+id+'_nav').removeClass('active'); //clearing
+            if (position >= target && targetBot >= position) { //if you are scrolled over element with some id 
+                $('.navbar-nav li#'+id+'_nav').addClass('active');
+                //this will check wich element you are scrolled on and selects in menu that item with same data-id :) 
+            }
+        });
+    });
+
+// fixing bottom nav to top on scrolliing
+var $fixednav = $(".bottom-nav .navbar-fixed-top");
+$(window).on("scroll", function () {
+    var $heightcalc = $(window).height() - $fixednav.height();
+    if ($(this).scrollTop() > $heightcalc) {
+        $fixednav.addClass("navbar-bottom-top");
+    } else {
+        $fixednav.removeClass("navbar-bottom-top");
+    }    
+});
+})
+function opensidebar(){
+    let menubars=document.querySelector('.menu_bars');
+    let pushmenu = document.querySelector('.pushmenu');
+    menubars.classList.toggle('active');
+    pushmenu.classList.toggle('pushmenu-open');
+}
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,162 @@
+
+jQuery(window).on("load", function () {
+    /* ===================================
+     Loading Timeout
+     ====================================== */
+
+    setTimeout(function () {
+        $("#loader").fadeOut("slow");
+    }, 3000);
+
+});
+
+jQuery(function ($) {
+
+    "use strict";
+//check for browser os
+    var isMobile = false;
+    var isiPhoneiPad = false;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        isMobile = true;
+    }
+
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        isiPhoneiPad = true;
+    }
+
+    /* ===================================
+     Animated Counter
+     ====================================== */
+
+
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+
+    /* ===================================
+     Animated Progress Bar
+     ====================================== */
+
+    $(".progress-bar").each(function () {
+        $(this).appear(function () {
+            $(this).animate({width: $(this).attr("aria-valuenow") + "%"}, 2000)
+        });
+    });
+
+
+    /* ===================================
+     Header Appear On Scroll
+     ====================================== */
+
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 70) { // Set position from top to add class
+            $('header').addClass('sticky header-appear');
+            $('.left-logo .navbar-brand').addClass("display_none");
+        }
+        else {
+            $('header').removeClass('sticky header-appear');
+            $('.left-logo .navbar-brand').removeClass("display_none");
+        }
+    });
+
+    // fixing bottom nav to top on scrolliing
+    var $fixednav = $(".bottom-nav .navbar-fixed-top");
+    $(window).on("scroll", function () {
+        var $heightcalc = $(window).height() - $fixednav.height();
+        if ($(this).scrollTop() > $heightcalc) {
+            $fixednav.addClass("navbar-bottom-top");
+        } else {
+            $fixednav.removeClass("navbar-bottom-top");
+        }
+    });
+
+
+
+    /* =====================================
+            Scroll
+     ====================================== */
+
+    //scroll to appear
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 150)
+            $('.scroll-top-arrow').fadeIn('slow');
+        else
+            $('.scroll-top-arrow').fadeOut('slow');
+    });
+    //Click event to scroll to top
+    $(document).on('click', '.scroll-top-arrow', function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
+
+    //scroll sections
+    $(".scroll").on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 750);
+    });
+
+
+    
+    /*==============================================================
+                equal Hieght
+      ==============================================================*/
+    var $window = $(window);
+    var windowsize = $(window).width();
+    checheight();
+    $window.on("resize", function () {
+        checheight();
+    });
+
+    function checheight() {
+        var $smae_height = $(".equalheight");
+        if ($smae_height.length) {
+            if (windowsize > 767) {
+                $smae_height.matchHeight({
+                    property: "height",
+                });
+            }
+        }
+    }
+
+    /* =====================================
+             Side Nav Absolute
+      ====================================== */
+
+    if ($("body").hasClass("side-nav")) {
+        var $menuLeft = $(".pushmenu-left");
+        var $menuRight = $(".pushmenu-right");
+        var $toggleleft = $(".menu_bars.left");
+        var $toggleright = $(".menu_bars.right");
+        $toggleright.on("click", function () {
+            $('.menu_bars').toggleClass("active");
+            $menuRight.toggleClass("pushmenu-open");
+            $("body").toggleClass("pushmenu-push-toLeft");
+            $(".navbar").toggleClass("navbar-right_open");
+            return false;
+        });
+
+        $('.push_nav li a').on('click', function () {
+            $toggleright.toggleClass("active");
+            $menuRight.toggleClass("pushmenu-open");
+            $("body").toggleClass("pushmenu-push-toLeft");
+            $(".navbar").toggleClass("navbar-right_open");
+            return true;
+        });
+    }
+
+
+
+
+   
+
+});
